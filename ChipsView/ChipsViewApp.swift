970fca4b88b8608371b3f45d3873337de928226e
Alex Bezkopylnyi <sanok.bes@gmail.com>
2023-08-22 20:15:29 +0100

    Initial Commit

---
+++ ChipsView/ChipsViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChipsViewApp.swift
+//  ChipsView
+//
+//  Created by AlexBezkopylnyi on 22.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChipsViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
